7b2bdee1dc06b5008740396e0de1834584457a6a
Sean Doyle <sean@ayerdoyles.org>
2016-12-14 17:50:06 -0500

    added color filteration

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 x = 1
-
+lowerLimit = cv2.scaleAdd(25,0,10)
+upperLimit = cv2.scaleAdd(75,255,245)
 while(x == 1):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
+    HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    newFrame = cv2.inRange(HSV,(25,0,10),(75,255,245))
+    cv2.imshow('frame', newFrame)
     if cv2.waitKey(30)& 0xFF == ord('q'):
         break
 cap.release()
