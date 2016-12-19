7fa39d6b9726f00ad4078ba51923f9e7a898ba63
Sean Doyle <sean@ayerdoyles.org>
2016-12-18 20:59:41 -0500

    kept making improvements to my openCV code

--- ShapeDrawingTest.py
+++ ShapeDrawingTest.py
@@ -2,11 +2,14 @@ import numpy as np
 import cv2
 
 img = np.zeros((512,512,3), np.uint8)
-font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.line(img,(0,0),(0,511),(255,0,0),511)
-cv2.rectangle(img,(10,10),(500,500),(0,255,0),-1)
-cv2.circle(img,(255,255),30,(0,0,255),-1)
-cv2.putText(img,'OpenCV',(20,450),font,4,(255,255,255),20,cv2.LINE_AA)
-cv2.imshow('Image', img)
-cv2.waitKey(0)
+def drawCircle(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),100,(255,255,255),10)
+
+cv2.namedWindow('Image')
+cv2.setMouseCallback('Image',drawCircle)
+while(1):
+    cv2.imshow('Image',img)
+    if cv2.waitKey(20) == 27:
+        break
 cv2.destroyWindow('Image')
--- test.py
+++ test.py
@@ -2,15 +2,19 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-x = 1
 lowerLimit = cv2.scaleAdd(25,0,10)
 upperLimit = cv2.scaleAdd(75,255,245)
-while(x == 1):
+erodedFrame = np.zeros((512,512,3), np.uint8)
+    
+while True:
     ret, frame = cap.read()
     HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     newFrame = cv2.inRange(HSV,(25,0,10),(75,255,245))
-    cv2.imshow('frame', newFrame)
-    if cv2.waitKey(30)& 0xFF == ord('q'):
+    erodedFrame = cv2.erode(newFrame,(300,300))
+    cv2.imshow('image', newFrame)
+    cv2.imshow('frame', erodedFrame)
+    if cv2.waitKey(5000)& 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyWindow('frame')
+cv2.destroyWindow('image')
